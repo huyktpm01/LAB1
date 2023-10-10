826f1982d33148f78e4d33ddcf7f8877541c7d56
huyktpm01 <102460104+huyktpm01@users.noreply.github.com>
2023-10-10 21:01:21 +0700

    a
    a

---
+++ SACHONLINE/Controllers/GioHangController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SACHONLINE.Models;
+namespace SACHONLINE.Controllers
+{
+    public class GioHangController : Controller
+    {
+        dbSachOnlineDataContext db = new dbSachOnlineDataContext("Data Source=LAPTOP-4PHTMN7E;Initial Catalog=SachOnline;Integrated Security=True");
+        // GET: GioHang
+        public List<GioHang> LayGioHang()
+        {
+            List<GioHang> lstGioHang = Session["GioHang"] as List<GioHang>;
+            if(lstGioHang == null)
+            {
+                lstGioHang= new List<GioHang>();
+                Session["GioHang"] = lstGioHang;
+            }
+            return lstGioHang;
+        }
+        public ActionResult ThemGioHang(int ms, string url) { 
+            List<GioHang> lstGioHang = LayGioHang();
+            GioHang sp = lstGioHang.Find(n => n.iMaSach == ms);
+            if(sp == null)
+            {
+                sp = new GioHang(ms);
+                lstGioHang.Add(sp);
+            }
+            else
+            {
+                sp.iSoLuong++;
+            }
+            return Redirect(url);
+        }
+        private int TongSoLuong()
+        {
+           int itongsoluong = 0;
+            List<GioHang> lstGioHang = Session["GioHang"] as List<GioHang>;
+            if( lstGioHang != null)
+            {
+                itongsoluong = lstGioHang.Sum(n => n.iSoLuong);
+            }
+            return itongsoluong;
+        }
+        private double TongTien()
+        {
+            double dtongtien = 0;
+            List<GioHang> lstGioHang = Session["GioHang"] as List<GioHang>;
+            if (lstGioHang != null)
+            {
+                dtongtien = lstGioHang.Sum(n => n.dThanhTien);
+            }
+            return dtongtien;
+        }
+        public ActionResult GioHang()
+        {
+            List<GioHang> lstGioHang = LayGioHang();
+            if(lstGioHang.Count == 0)
+            {
+                return RedirectToAction("Index", "SachOnline");
+            }
+            ViewBag.TongSoLuong = TongSoLuong();
+            ViewBag.TongTien = TongTien();
+            return View(lstGioHang);
+        }
+        public ActionResult GioHangPartial()
+        {
+            ViewBag.TongSoLuong = TongSoLuong();
+            ViewBag.TongTien = TongTien();
+            return PartialView();
+        }
+        public ActionResult XoaSPKhoiGioHang(int iMaSach)
+        {
+            List<GioHang> lstGioHang = LayGioHang();
+            GioHang sp = lstGioHang.SingleOrDefault(n => n.iMaSach == iMaSach);
+            if(sp != null)
+            {
+
+                lstGioHang.RemoveAt(lstGioHang.IndexOf(sp));
+                if(lstGioHang.Count == 0)
+                {
+                    return RedirectToAction("Index", "SachOnline");
+                }
+            }
+            return RedirectToAction("GioHang");
+        }
+        public ActionResult CapNhatGioHang(int iMaSach,FormCollection f) {
+            List<GioHang> lstGioHang = LayGioHang();
+            GioHang sp = lstGioHang.SingleOrDefault(n => n.iMaSach == iMaSach);
+            if (sp != null)
+            {
+                sp.iSoLuong = int.Parse(f["txtSoLuong"].ToString());
+            }
+            return RedirectToAction("GioHang");
+        }
+        public ActionResult XoaGioHang()
+        {
+            List<GioHang> lstGioHang = LayGioHang();
+            lstGioHang.Clear();
+            return RedirectToAction("Index","SachOnline");
+        }
+        [HttpGet]
+        public ActionResult DatHang()
+        {
+            if (Session["TaiKhoan"] == null|| Session["TaiKhoan"].ToString() == "")
+            {
+                return RedirectToAction("DangNhap", "User");
+            }
+            if (Session["GioHang"] == null)
+            {
+                return RedirectToAction("Index", "SachOnline");
+            }
+            List<GioHang> ltsgiohang = LayGioHang();
+            ViewBag.TongSoLuong = TongSoLuong();
+            ViewBag.TongTien = TongTien();
+            return View(ltsgiohang);
+        }
+        [HttpPost]
+        public ActionResult DatHang(FormCollection f)
+        {
+
+            DONDATHANG ddh = new DONDATHANG();
+            KHACHHANG kh = (KHACHHANG)Session["Taikhoan"];
+            List<GioHang> lstGioHang = LayGioHang();
+            ddh.MaKH = kh.MaKH;
+            ddh.NgayDat = DateTime.Now;
+           
+            var NgayGiao = String.Format("{0:MM/dd/yyyy}", f["NgayGiao"]);
+            ddh.NgayGiao = DateTime.Parse(NgayGiao);
+            ddh.TinhTrangGiaoHang = 1;
+            ddh.DaThanhToan = false;
+            db.DONDATHANGs.InsertOnSubmit(ddh);
+            db.SubmitChanges();
+            //Thém chi tiét don hang
+            foreach (var item in lstGioHang)
+            {
+                CHITIETDATHANG ctdh = new CHITIETDATHANG();
+                ctdh.MaDonHang = ddh.MaDonHang;
+                ctdh.MaSach = item.iMaSach;
+                ctdh.SoLuong = item.iSoLuong;
+                ctdh.DonGia = (decimal)item.dDonGia;
+                db.CHITIETDATHANGs.InsertOnSubmit(ctdh);
+
+
+            }
+            db.SubmitChanges();
+            Session["GioHang"] = null;
+            return RedirectToAction("XacNhanDonHang", "GioHang");
+        }
+
+        public ActionResult XacNhanDonHang()
+        {
+            return View();
+        }
+    }
+}
---
+++ SACHONLINE/Controllers/UserController.cs
@@ -0,0 +1,118 @@
+﻿using SACHONLINE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SACHONLINE.Controllers
+{
+    public class UserController : Controller
+    {
+        dbSachOnlineDataContext db = new dbSachOnlineDataContext("Data Source=LAPTOP-4PHTMN7E;Initial Catalog=SachOnline;Integrated Security=True");
+        // GET: User
+        [HttpGet]
+        public ActionResult DangKy()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DangKy(FormCollection collection, KHACHHANG kh)
+        {
+            var sHoten = collection["HoTen"];
+            var sTenDN = collection["TenDN"];
+            var sMatKhau = collection["MatKhau"];
+            var sMatKhauNL = collection["MatKhauNL"];
+            var sDiaChi = collection["DiaChi"];
+            var sEmail = collection["Email"];
+            var sDienThoai= collection["DienThoai"];
+            var sNgaySinh = String.Format("{0:MM/dd/yyyy}",collection["NgaySinh"]);
+            if (String.IsNullOrEmpty(sHoten))
+            {
+                ViewData["err1"] = "Họ tên không được rỗng";
+            }
+            else if(String.IsNullOrEmpty(sTenDN))
+            {
+                ViewData["err2"] = "Tên đăng nhập không được rỗng";
+            }
+            else if (String.IsNullOrEmpty(sMatKhau))
+            {
+                ViewData["err3"] = "Phải Nhập mật khẩu";
+            }
+            else if (String.IsNullOrEmpty(sMatKhauNL))
+            {
+                ViewData["err4"] = "Phải nhập lại mật khẩu";
+            }
+            else if (sMatKhau != sMatKhauNL)
+            {
+                ViewData["err4"] = "Mật khẩu không khớp";
+            }
+            else if (String.IsNullOrEmpty(sEmail))
+            {
+                ViewData["err5"] = "Email không được rỗng";
+            }
+            else if (String.IsNullOrEmpty(sDienThoai))
+            {
+                ViewData["err6"] = "Điện Thoại không được rỗng";
+            }
+            else if (db.KHACHHANGs.Any(n => n.TaiKhoan == sTenDN))
+            {
+                ViewBag.ThongBao = "Tên đăng nhập đã tồn tại";
+            }
+            else if (db.KHACHHANGs.Any(n => n.Email == sEmail))
+            {
+                ViewBag.ThongBao = "Email đã được sử dụng";
+            }
+            else
+            {
+                kh.HoTen = sHoten;
+                kh.TaiKhoan = sTenDN;
+                kh.MatKhau = sMatKhau;
+                kh.Email = sEmail;
+                kh.DiaChi = sDiaChi;
+                kh.DienThoai = sDienThoai;
+                kh.NgaySinh = DateTime.Parse(sNgaySinh);
+
+                db.KHACHHANGs.InsertOnSubmit(kh);
+                db.SubmitChanges();
+                return RedirectToAction("DangNhap");
+            }
+            return this.DangKy();
+        }
+        [HttpGet]
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DangNhap(FormCollection collection)
+        {
+            var sTenDN = collection["TenDN"];
+            var sMatKhau = collection["Matkhau"];
+            if (String.IsNullOrEmpty(sTenDN))
+            {
+                ViewData["Err1"] = "Bạn chưa nhập tên đăng nhập";
+            }
+            else if (String.IsNullOrEmpty(sMatKhau))
+            {
+                ViewData["Err2"] = "Phải nhập mật khẩu";
+            }
+            else
+            {
+                KHACHHANG kh = db.KHACHHANGs.SingleOrDefault(n => n.TaiKhoan == sTenDN && n.MatKhau == sMatKhau);
+                if (kh != null)
+                {
+                    ViewBag.ThongBao = "Chúc mừng đăng nhập thành công";
+                    Session["TaiKhoan"] = kh;
+                }
+                else
+                {
+                    ViewBag.ThongBao = "Tên đăng nhập hoặc mật khẩu không đúng";
+                }
+
+            }
+            return View();
+
+        }
+    }
+}
